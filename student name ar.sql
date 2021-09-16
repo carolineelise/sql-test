0cd635f659ddcd2f507ce6fc3b88333a467ab825
unknown <carolineelise616@gmail.com>
2021-09-16 15:10:23 -0400

    inital commit

---
+++ student name ar.sql
@@ -0,0 +1,2 @@
+select * from Student
+where Firstname like '%ar%';
---
+++ test.sql
@@ -0,0 +1,7 @@
+select concat(firstname,' ',lastname) as 'name', sat, concat(gpa,'       ', id) as 'score', from student 
+where sat >= 1490
+order by GPA desc, sat desc; select statecode from Student;
+select id, code from Major;
+
+
+
