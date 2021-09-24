d6a2d56647e72c320eefc278c4124b7e2baeca83
carolineelise <90719451+carolineelise@users.noreply.github.com>
2021-09-24 10:17:23 -0400

    Delete test join - Copy.sql

--- test join - Copy.sql
+++
@@ -1,9 +0,0 @@
-select Description, c.Code, Subject, Section
-from Major m
-join MajorClass mc
-on mc.MajorId = m.Id
-join Class c
-on c.Id = mc.ClassId
-where m.Code = 'GENB';
-
-select * from class;
